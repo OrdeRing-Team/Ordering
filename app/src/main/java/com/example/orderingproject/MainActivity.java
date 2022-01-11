6495183bc6b58df9360f290336d4e4bb80caa7fe
minju park <mlnjv016@gmail.com>
2022-01-11 18:22:50 +0900

    Ordering 소비자 앱 네비바 구현

--- app/src/main/java/com/example/orderingproject/MainActivity.java
+++ app/src/main/java/com/example/orderingproject/MainActivity.java
@@ -1,15 +1,49 @@
 package com.example.orderingproject;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
-    // push test
-    // 22222
+
+    BottomNavigationView bottomNavigationView; //바텀네비뷰
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottomNavi);
+
+        //처음화면
+        getSupportFragmentManager().beginTransaction().add(R.id.main_frame, new HomeFragment()).commit(); //FrameLayout에 QrFragment.xml띄우기
+
+        //바텀 네비게이션뷰 안의 아이템 설정
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuitem) {
+                switch (menuitem.getItemId()) {
+                    //item 클릭 시 id값을 가져와 FrameLayout에 fragment.xml 띄우기
+                    case R.id.item_home:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.main_frame, new HomeFragment()).commit();
+                        break;
+                    case R.id.item_orderlist:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.main_frame, new OrderlistFragment()).commit();
+                        break;
+                    case R.id.item_waiting:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.main_frame, new WaitingFragment()).commit();
+                        break;
+                    case R.id.item_mypage:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.main_frame, new MypageFragment()).commit();
+                        break;
+                }
+                return true;
+            }
+        });
+
     }
 }
