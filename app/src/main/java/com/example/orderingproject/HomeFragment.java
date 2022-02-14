c00a2dd68ce05b8c402c7039bc7877979322a775
seunggyu97 <seunggyu97@gmail.com>
2022-02-15 03:48:05 +0900

    로그아웃, 회원탈퇴 기능 임시 적용

--- app/src/main/java/com/example/orderingproject/HomeFragment.java
+++ app/src/main/java/com/example/orderingproject/HomeFragment.java
@@ -1,19 +1,104 @@
 package com.example.orderingproject;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.example.orderingproject.databinding.FragmentHomeBinding;
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class HomeFragment extends Fragment {
+
+    private View view;
+
+    private FragmentHomeBinding binding;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        binding = FragmentHomeBinding.inflate(inflater, container, false);
+
+        view = binding.getRoot();
+
+        initButtonClickListener();
+
+        return view;
+    }
+
+    private void initButtonClickListener(){
+        binding.btnLogout.setOnClickListener(onClickListener);
+        binding.btnDeleteAccount.setOnClickListener(onClickListener);
+    }
+    View.OnClickListener onClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            switch(view.getId()){
+                case R.id.btn_logout:
+                    logout();
+                    break;
+                case R.id.btn_deleteAccount:
+                    deleteAccount();
+                    break;
+            }
+        }
+    };
+
+    /* 로그아웃 */
+    public void logout() {
+        FirebaseAuth.getInstance().signOut();
+
+        startActivity(new Intent(getActivity(), StartActivity.class));
+        Toast.makeText(getActivity(), "로그아웃 되었습니다.", Toast.LENGTH_SHORT).show();
+        getActivity().finish();
+    }
+
+    /* 회원탈퇴 */
+    public void deleteAccount() {
+        FirebaseAuth.getInstance().getCurrentUser().delete();
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        AuthUI.getInstance()
+                .delete(getActivity())
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        // 유저 DB 삭제
+                        db.collection("users").document(user.getUid())
+                                .delete()
+                                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                    @Override
+                                    public void onSuccess(Void aVoid) {
+                                        Log.d("유저DB 삭제", "성공");
+                                    }
+                                })
+                                .addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        Log.w("유저DB 삭제", "실패", e);
+                                    }
+                                });
+                        Toast.makeText(getActivity(), "회원탈퇴가 정상적으로 처리되었습니다.", Toast.LENGTH_LONG).show();
+                        FirebaseAuth.getInstance().signOut();
+                        getActivity().finish();
+                        startActivity(new Intent(getActivity(), StartActivity.class));
+                    }
+                });
+
     }
 }
--- app/src/main/java/com/example/orderingproject/LoginActivity.java
+++
@@ -1,5 +0,0 @@
-package com.example.orderingproject;
-
-public class LoginActivity {
-
-}
