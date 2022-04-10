b64a03cd7094f107c6b59bf55b167aa6f3920b7c
Minju <mlnjv016@gmail.com>
2022-04-10 20:00:48 +0900

    1. fragment_home.xml 디자인 2. fragment_mypage.xml, MypageFragment로 로그아웃, 회원가입 이동 3. activity_start.xml 상단 로고 삭제

--- app/src/main/java/com/example/orderingproject/HomeFragment.java
+++ app/src/main/java/com/example/orderingproject/HomeFragment.java
@@ -43,74 +43,11 @@ public class HomeFragment extends Fragment {
 
         view = binding.getRoot();
 
-        initButtonClickListener();
 
         return view;
     }
 
-    private void initButtonClickListener(){
-        binding.btnLogout.setOnClickListener(onClickListener);
-        binding.btnDeleteAccount.setOnClickListener(onClickListener);
-    }
-    View.OnClickListener onClickListener = new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-            switch(view.getId()){
-                case R.id.btn_logout:
-                    logout();
-                    break;
-                case R.id.btn_deleteAccount:
-                    deleteAccount();
-                    break;
-            }
-        }
-    };
 
-
-
-    /* 로그아웃 */
-    public void logout() {
-        FirebaseAuth.getInstance().signOut();
-
-        startActivity(new Intent(getActivity(), StartActivity.class));
-        Toast.makeText(getActivity(), "로그아웃 되었습니다.", Toast.LENGTH_SHORT).show();
-        getActivity().finish();
-    }
-
-    /* 회원탈퇴 */
-    public void deleteAccount() {
-        FirebaseAuth.getInstance().getCurrentUser().delete();
-        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-        FirebaseFirestore db = FirebaseFirestore.getInstance();
-
-        AuthUI.getInstance()
-                .delete(getActivity())
-                .addOnCompleteListener(new OnCompleteListener<Void>() {
-                    @Override
-                    public void onComplete(@NonNull Task<Void> task) {
-                        // 유저 DB 삭제
-                        db.collection("users").document(user.getUid())
-                                .delete()
-                                .addOnSuccessListener(new OnSuccessListener<Void>() {
-                                    @Override
-                                    public void onSuccess(Void aVoid) {
-                                        Log.d("유저DB 삭제", "성공");
-                                    }
-                                })
-                                .addOnFailureListener(new OnFailureListener() {
-                                    @Override
-                                    public void onFailure(@NonNull Exception e) {
-                                        Log.w("유저DB 삭제", "실패", e);
-                                    }
-                                });
-                        Toast.makeText(getActivity(), "회원탈퇴가 정상적으로 처리되었습니다.", Toast.LENGTH_LONG).show();
-                        FirebaseAuth.getInstance().signOut();
-                        getActivity().finish();
-                        startActivity(new Intent(getActivity(), StartActivity.class));
-                    }
-                });
-
-    }
 
 
 }
