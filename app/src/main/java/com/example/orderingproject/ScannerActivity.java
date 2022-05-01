3b82493bbe4ceb6f21931538db95c206bf5a942d
seunggyu <seunggyu97@gmail.com>
2022-05-02 03:48:52 +0900

    1. [ScannerActivity] QR스캔 라이트 구현

--- app/src/main/java/com/example/orderingproject/ScannerActivity.java
+++ app/src/main/java/com/example/orderingproject/ScannerActivity.java
@@ -1,7 +1,13 @@
 package com.example.orderingproject;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.content.res.AppCompatResources;
 
 import com.bumptech.glide.Glide;
 import com.example.orderingproject.databinding.ActivityQrscannerBinding;
@@ -14,6 +20,12 @@ public class ScannerActivity extends BasicActivity{
     private ActivityQrscannerBinding binding;
 
     CaptureManager captureManager;
+    boolean flashStatus = false;
+    Drawable lightOn;
+    Drawable lightOff;
+    Drawable lightOnButton;
+    Drawable lightOffButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
@@ -23,16 +35,53 @@ public class ScannerActivity extends BasicActivity{
 
         initViews();
         initData(savedInstanceState);
+        initClickListeners();
     }
 
+    @SuppressLint("UseCompatLoadingForDrawables")
     private void initData(Bundle savedInstanceState){
         captureManager = new CaptureManager(this,binding.scanBox);
         captureManager.initializeFromIntent(this.getIntent(), savedInstanceState);
         captureManager.decode();
+
+        lightOn = getDrawable(R.drawable.light_on);
+        lightOff = getDrawable(R.drawable.light_off);
+        lightOnButton = getDrawable(R.drawable.circle_button_light_on);
+        lightOffButton = getDrawable(R.drawable.circle_button);
     }
 
     private void initViews(){
         Glide.with(this).load(R.drawable.qr_scan_animation_white).into(binding.ivScanAnim);
+    }
+
+    private void initClickListeners(){
+        binding.btnFlash.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(!flashStatus){
+                    binding.scanBox.setTorchOn();
+                    binding.ivFlash.setImageDrawable(lightOn);
+                    binding.btnFlash.setBackground(lightOnButton);
+                    MainActivity.showToast(ScannerActivity.this, "라이트 On");
+                    flashStatus = true;
+                }
+                else{
+                    binding.scanBox.setTorchOff();
+                    binding.ivFlash.setImageDrawable(lightOff);
+                    binding.btnFlash.setBackground(lightOffButton);
+                    binding.btnFlash.setBackground(AppCompatResources.getDrawable(getApplicationContext(), R.drawable.circle_button));
+                    MainActivity.showToast(ScannerActivity.this, "라이트 Off");
+                    flashStatus = false;
+                }
+            }
+        });
+
+        binding.btnClose.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                finish();
+            }
+        });
     }
 
     @Override
